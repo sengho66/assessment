69d6b1a1dc77b2f6ee45babea58d20b43ffb5f5c
sengho66 <htungseng@gmail.com>
2024-04-29 07:35:15 +0800

    commit message

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Table from "./components/table";
+import styles from "./page.module.css";
+import { getBooks } from "./utils";
+
+export default async function Home() {
+  const books = await getBooks();
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <Table books={books} />
+      </div>
+    </main>
+  );
+}
